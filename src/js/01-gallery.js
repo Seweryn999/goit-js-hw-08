1084de97db7e8e9d9c081e31fe80cc902daa5a9e
Seweryn <sewcio0212@gmail.com>
2024-04-12 19:46:02 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,54 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+
+const link = document.querySelector('.gallery');
+
+for (const obj of galleryItems) {
+  const div = document.createElement('div');
+  div.classList.add('gallery__item');
+
+  const anchor = document.createElement('a');
+  anchor.classList.add('gallery__link');
+  anchor.setAttribute('href', obj.original);
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = obj.preview;
+  image.setAttribute('data-source', obj.original);
+  image.alt = obj.description;
+
+  link.appendChild(div);
+  div.appendChild(anchor);
+  anchor.appendChild(image);
+}
+
+link.addEventListener('click', showImage);
+
+function showImage(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src='${event.target.dataset.source}'/>`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', closeModal);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', closeModal);
+      },
+    }
+  );
+  instance.show();
+
+  function closeModal(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
 
 console.log(galleryItems);
