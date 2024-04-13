eb361b64bc41be590aa37e7d4789c666e8f29843
Seweryn <sewcio0212@gmail.com>
2024-04-13 09:01:09 +0200

    required added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const updateTimeInLocalStorage = throttle(async () => {
+  const currentTime = await player.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', updateTimeInLocalStorage);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
