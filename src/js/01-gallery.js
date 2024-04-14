1271ddc622933784d8189bcf613cedc81876b93b
Seweryn <sewcio0212@gmail.com>
2024-04-14 13:43:51 +0200

    files back to src folder

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
-// import SimpleLightbox from 'simplelightbox';
-// import SimpleLightbox from '/node_modules/simplelightbox/dist/simple-lightbox.esm.js';
+import { galleryItems } from './gallery-items.js';
 
-import { galleryItems } from './gallery-items';
+console.log(galleryItems);
 
 const link = document.querySelector('.gallery');
 
