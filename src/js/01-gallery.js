a9a9bf0165e6f06f7dc56407320abd08eeb4141c
Seweryn <sewcio0212@gmail.com>
2024-04-15 17:01:30 +0200

    node.js allowed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,16 @@
+// Opisany w dokumentacji
 import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
 console.log(galleryItems);
-
 const link = document.querySelector('.gallery');
 
 for (const obj of galleryItems) {
-  const div = document.createElement('div');
-  div.classList.add('gallery__item');
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
 
   const anchor = document.createElement('a');
   anchor.classList.add('gallery__link');
@@ -18,15 +20,17 @@ for (const obj of galleryItems) {
   image.classList.add('gallery__image');
   image.src = obj.preview;
   image.setAttribute('data-source', obj.original);
-  image.alt = obj.description;
+  image.title = obj.description;
 
-  link.appendChild(div);
-  div.appendChild(anchor);
+  link.appendChild(li);
+  li.appendChild(anchor);
   anchor.appendChild(image);
 }
 
 var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 link.addEventListener('click', event => {
-  event.preventDefault();
+  if (event.target.tagName === 'IMG') {
+    event.preventDefault();
+  }
 });
